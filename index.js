71ff89332b2389ad170f62aedb4b95f21177d19f
Liz S <liz.shigetoshi@mapbox.com>
2018-02-19 10:46:29 -0800

    Change available() to feature detection

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 /**
- * Detect whether a browser supports the execCommand interface
- * for copying text.
+ * Detect if there is browser support for the copy command feature.
  *
- * @returns {boolean} whether the browser support text
+ * @returns {boolean} whether or not the browser supports copy
  */
 function available() {
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Browser_detection_using_the_user_agent
-  // https://developer.mozilla.org/en-US/docs/Web/API/Window/navigator
-  // https://developer.mozilla.org/en-US/docs/Web/API/Document/execCommand
-  var browser = navigator.userAgent.match(/(Firefox|Chrome|Safari|Opera)\/(\d+)/);
-  return 'execCommand' in document && !!(browser && {
-    Firefox: parseInt(browser[2], 10) >= 41,
-    Chrome: parseInt(browser[2], 10) >= 43,
-    Safari: parseInt(browser[2], 10) >= 10,
-    Opera: parseInt(browser[2], 10) >= 29
-  }[browser[1]]);
+  let isCopySupported = !!document.queryCommandSupported;
+  isCopySupported = isCopySupported && !!document.queryCommandSupported('copy');
+  return isCopySupported;
 }
 
 /**
